6a834bd66340682023ebd750f0af2465a59cc6d1
Ivan D <DivanDoesCode@gmail.com>
2019-03-27 17:53:50 +0300

    addTodo and toggle todo added

--- src/store/reducers/todos.tsx
+++ src/store/reducers/todos.tsx
@@ -1,16 +1,47 @@
 /*
  *     REDUCERS: Todos
  */
-interface todos {
-  type?: string;
-  id?: number;
-  task?: string;
+interface todo {
+  id: number;
+  text: string;
+  isCompleted: boolean;
 }
 
-const initialState = {};
+interface actionTodo extends todo {
+  type: string;
+}
 
-export const todos = (state: todos = initialState, action: todos) => {
+const initialState = [];
+
+export const todos = (state: todo[] = initialState, action: actionTodo) => {
   switch (action.type) {
+    case "ADD_TODO":
+      return [...state, todo(undefined, action)];
+
+    case "TOGGLE_TODO":
+      return state.map(task => todo(task, action));
+    default:
+      return state;
+  }
+};
+
+/*
+ *     REDUCERS: Single Todo
+ */
+const todo = (state, action: actionTodo) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        id: action.id,
+        text: action.text,
+        isCompleted: action.isCompleted
+      };
+    case "TOGGLE_TODO":
+      if (state.id !== action.id) return state;
+      return {
+        ...state,
+        isCompleted: !state.isCompleted
+      };
     default:
       return state;
   }
