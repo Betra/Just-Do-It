e3bcbe0e02a0c195ffb139399a788027f2107be9
Ivan D <DivanDoesCode@gmail.com>
2019-03-30 18:58:51 +0300

    VisibleTodoList now functioning via connect

--- src/components/VisibleTodoList/VisibleTodoList.tsx
+++ src/components/VisibleTodoList/VisibleTodoList.tsx
@@ -1,16 +1,34 @@
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
+
+import { Todo } from "../../store/interfaces";
+
 import { TodoList } from "../TodoList";
+
 import { toggleTodo, getVisibleTodos } from "../../actions";
 
-const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+interface StoreProps {
+  todos: Todo[] | undefined;
+}
+
+interface DispatchProps {
+  onTodoClick: (id: number) => any;
+}
+
+const mapStateToProps = (state): StoreProps => ({
+  todos: getVisibleTodos(state.todos, state.filter)
 });
 
-const mapDispatchToProps = ({ dispatch }) => ({
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   onTodoClick: id => dispatch(toggleTodo(id))
 });
 
-export const VisibleTodoList = connect(
+interface Props {
+  todos?: any;
+  onTodoClick?: any;
+}
+
+export const VisibleTodoList = connect<Props>( // Я даже спрашивать не буду, почему это работае, ушло 40 минут
   mapStateToProps,
   mapDispatchToProps
 )(TodoList);
