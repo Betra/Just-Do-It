093d6f66d63fdf938a9d7549bc66825a902ec5f1
Ivan D <DivanDoesApps@gmail.com>
2019-04-05 20:40:43 +0300

    Utils added

---
+++ src/utils/index.tsx
@@ -0,0 +1,6 @@
+export { parseDate } from "./parseDate";
+export { parseMonthToName } from "./parseMonthToName";
+export { shortify } from "./shortify";
+export { getAmountOfDaysInMonth } from "./getAmountOfDaysInMonth";
+export { getFirstDayInAMonth } from "./getFirstDayInAMonth";
+export { range } from "./range";
